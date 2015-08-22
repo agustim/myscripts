498e565e4bae546c63bf67c9007119e5f1dbe686
Agustí Moll <agusti.moll@guifi.net>
2015-08-22 12:23:41 +0200

    Lock and unlock screen

--- system.sh
+++ system.sh
@@ -1,9 +1,7 @@
 lock_screen(){
- dbus-send --type=method_call --dest=org.gnome.ScreenSaver \
-    /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock
+ /usr/bin/dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock
 }
 
 unlock_screen(){
-  export DISPLAY=:0; 
-  dbus-send --session --dest=org.gnome.ScreenSaver --type=method_call --print-reply --reply-timeout=20000 /org/gnome/ScreenSaver org.gnome.ScreenSaver.SetActive boolean:false
+  export DISPLAY=:0 && /usr/bin/dbus-send --session --dest=org.gnome.ScreenSaver --type=method_call --print-reply --reply-timeout=20000 /org/gnome/ScreenSaver org.gnome.ScreenSaver.SetActive boolean:false
 }
