a888cfb7c78120979784f9173075510bba99fa76
agusti <agusti@gmail.com>
2017-09-04 12:43:35 +0200

    add second fullhd screen

--- hdmi.sh
+++ hdmi.sh
@@ -4,3 +4,8 @@ scan-home() {
 sudo xrandr --output eDP1 --transform 1,0,0,0,1,0,0,0,1
 sudo xrandr --output HDMI1 --transform 1,0,-59,0,1,-25,0,0,1
 }
+
+display-omnium() {
+	xrandr --output HDMI1 --scale 2x2 --auto --panning 3840x2160+3200+0 --right-of eDP1
+	xrandr --output eDP1 --auto --scale 1x1
+}
